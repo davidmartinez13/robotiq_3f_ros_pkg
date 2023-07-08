306f380c4244fd71d76b2f1ea2fc4cf11269e30b
davidmartinez13 <dsmartinez13@hotmail.com>
2023-07-08 19:15:50 +0200

    added gripper activation

--- robotiq_3f_driver/src/activate.py
+++ robotiq_3f_driver/src/activate.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     rospy.loginfo('waiting for robotiq 3f gripper activate.')
     rospy.wait_for_service('/robotiq_3f_gripper/activate')
     try:
-        # activate = rospy.ServiceProxy('/robotiq_3f_gripper/activate', Activate)
-        # ret = activate()
+        activate = rospy.ServiceProxy('/robotiq_3f_gripper/activate', Activate)
+        ret = activate()
         rospy.loginfo('robotiq 3f gripper activation complete.')
     except rospy.ServiceException as e:
         rospy.logerr('Activation service failed:{}'.format(e))
